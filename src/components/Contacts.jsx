d9686b2f1d0bedbca1cb62edfb18b023a48c3fbc
nayamarsha <nayamarsha04@gmail.com>
2025-11-12 13:55:49 +0700

    .

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -39,7 +39,7 @@ function Contacts() {
       id="contacts"
       style={{
         background: "#F5F0E8",
-        height: "85vh",
+        height: "100vh",
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
